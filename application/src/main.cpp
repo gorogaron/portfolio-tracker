c887b3d8c8d4aab4c223ac23a6e322e8ef16b8fe
gorogaron <gorogaron95@gmail.com>
2022-10-05 21:45:26 +0000

    Add httplib dependency

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 #include "postgresql/libpq-fe.h"
+#include <httplib.h>
 
 int main(){
+    // Test PostgreSQL connection
     PGconn* wCon;
-
     wCon = PQconnectdb("");
-
     if (PQstatus(wCon) != CONNECTION_OK)
     {
         std::cout << "Connection failed!" << std::endl;
@@ -16,6 +16,12 @@ int main(){
     {
         std::cout << "Connection established!" << std::endl;
     }
-
     PQfinish(wCon);
+
+    //Test finnhub API
+    httplib::Client cli("https://finnhub.io");
+    std::string apiKey = "1234";
+    auto res = cli.Get("/api/v1/quote?symbol=AAPL&token=" + apiKey);
+    std::cout << res->body << std::endl;
+
 }
